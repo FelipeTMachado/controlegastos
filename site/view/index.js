5e16c85f43304abee4edd55c2c5b5f6449c1d1b9
yohanes <yzanghelini@gmail.com>
2025-01-15 17:04:13 -0300

    criando formulario de transacao

---
+++ site/view/index.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('formTransacao');
+
+function showForm() {
+    form.classList.remove('hidden');
+}
+
+function hideForm() {
+    form.classList.add('hidden');
+}
--- site/view/principal.php
+++ site/view/principal.php
@@ -76,7 +76,7 @@
 
     <!-- BOTÃO PARA ADICIONAR NOVAS TRANSACOES -->
     <div class="mt-8 text-center">
-      <button type="" class="bg-gray-600 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-700">
+      <button type="" class="bg-gray-600 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-700" onclick="showForm()">
         Adicionar Transação
       </button>
     </div>
@@ -86,5 +86,41 @@
   <footer class="bg-gray-900 text-white py-4 text-center">
     <p>&copy; 2024 Controle de Gastos. Todos os direitos reservados.</p>
   </footer>
+
+
+  <div class="fixed hidden inset-0 flex items-center justify-center z-50 backdrop-blur-sm" id="formTransacao">
+
+    <div class="bg-gray-900 p-4 flex rounded-lg shadow w-1/4 h-1/2 flex justify-center items-center">
+      
+      <form action="" class="formTransacao flex flex-col">
+        <div class="flex flex-col"> <!-- Adicionando flex e gap -->
+          <label for="descricaoTransacao" class="text-white">Descrição</label>
+          <input type="text" class="inputTransacao w-full px-4 py-1 bg-gray-700 border border-gray-600 rounded-lg text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" id="descricaoTransacao" name="descricaoTransacao" required placeholder="Descrição da transação">
+        </div>
+
+        <div class="flex flex-col"> <!-- Adicionando flex e gap -->
+          <label for="valorTransacao" class="text-white mt-4">Valor (R$)</label>
+          <input type="number" class="inputTransacao w-full px-4 py-1 bg-gray-700 border border-gray-600 rounded-lg text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" id="valorTransacao" name="valorTransacao" required placeholder="R$0,00">
+        </div>
+
+        <div class="flex mt-4 justify-around"> <!-- Adicionando flex e gap -->
+          <div class="border-green-900 border p-2 rounded-lg">
+            <label for="adicionar" class="text-white">Adicionar</label>
+            <input type="radio" id="adicionar" name="tipoTransacao" value="adicionar">
+          </div>
+
+          <div class="border border-red-900 p-2 rounded-lg">
+            <label for="depositar" class="text-white">Depositar</label>
+            <input type="radio" id="depositar" name="tipoTransacao" value="depositar">
+          </div>          
+        </div>
+
+        <button type="submit" onclick="hideForm()" class="mt-4 bg-gray-600 text-white px-1 py-1 rounded-lg shadow hover:bg-blue-700">Concluir</button>
+      </form>
+
+    </div>
+  </div>
+
+  <script src="index.js"></script>
 </body>
 </html>
